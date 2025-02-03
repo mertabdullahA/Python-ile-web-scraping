28ae286087dc39dce2a65ebec37d88cfc2ef9fa6
ABdullah Mert <abdullahmertcankaya@gmail.com>
2025-02-03 22:02:56 +0300

    değişiklik  yapıldı

--- web scraping/scraping.py
+++ web scraping/scraping.py
@@ -3,22 +3,17 @@ from bs4 import BeautifulSoup
 
 def laptop_ozelliklerini_getir(url):
     try:
-        # URL'ye HTTP isteği gönderir
         response=requests.get(url)
-        response.raise_for_status() # HTTP hatalarını kontrol eder
+        response.raise_for_status() 
 
-        # HTML içeriğini işlemek için BeautifulSoup kullanır
         soup=BeautifulSoup(response.text, "html.parser")
         
-        # Örnek: Laptop özelliklerini içeren bir HTML sınıfını bul
-        # Bu, sitenin yapısına bağlı olarak değişecektir
         ozellikler=soup.find('div', class_='product-list__content product-detail-big-price')
         
         if not ozellikler:
             print("Laptop özellikleri bulunamadı. Farklı bir URL deneyin.")
             return
         
-        # Özellikleri yazdır
         print("Laptop Özellikleri:")
         for özellik in ozellikler:
             print(özellik.get_text(strip=True))
